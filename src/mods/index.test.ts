de1a2458edc9732a2da25cd6e6f622a14f6b561c
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-31 22:30:07 +0700

    d

--- src/mods/index.test.ts
+++ src/mods/index.test.ts
@@ -1,8 +1,7 @@
-import * as ed25519c from "@noble/curves/ed25519"
-import * as ed25519 from "@noble/ed25519"
 import { fromNoble } from "./ed25519/noble.js"
 
-console.log("no test")
+import * as ed25519c from "@noble/curves/ed25519"
+import * as ed25519 from "@noble/ed25519"
 
 fromNoble({ ed25519 })
 fromNoble(ed25519c)
